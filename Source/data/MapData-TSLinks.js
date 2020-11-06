486a26038a7708923e5ee8727c07981e9f26815d
Simon Jerabeck <simon@hurix.de>
2020-11-06 05:32:35 +0100

    js async stuff is weird as fuck. code is ugly but works. needs better data

--- Source/data/MapData-TSLinks.js
+++ Source/data/MapData-TSLinks.js
@@ -1,4 +1,5 @@
 const API_ENDPOINT = `https://api.canonn.tech`;
+const EDSM_ENDPOINT = `https://www.edsm.net/api-v1`;
 const API_LIMIT = 1000;
 
 const capi = axios.create({
@@ -12,6 +13,14 @@ const capi = axios.create({
 let sites = {
 	tssites: [],
 };
+
+const edsmapi = axios.create({
+    baseURL: EDSM_ENDPOINT,
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+})
 
 const go = async types => {
 	const keys = Object.keys(types);
@@ -48,10 +57,20 @@ const reqSites = async (API_START, type) => {
 	return payload;
 };
 
-const reqSites = async (API_START, type) => {
+const reqSystemName = async (name) => {
 
 	let payload = await capi({
-		url: `/${type}?_limit=${API_LIMIT}&_start=${API_START}`,
+		url: `/systems?systemName=${name}`,
+		method: 'get'
+	});
+
+	return payload;
+};
+
+const getSystemEDSM = async (systemName) => {
+
+	let payload = await edsmapi({
+		url: `/systems?systemName=${systemName}&showCoordinates=1`,
 		method: 'get'
 	});
 
@@ -89,11 +108,11 @@ var canonnEd3d_tslinks = {
                 },
                 '40': {
                     name: 'Thargoid Site',
-                    color: '66ff66',
+                    color: '003300',
                 },
                 '60': {
                     name: 'Eagle Eye',
-                    color: '6666ff',
+                    color: '000033',
                 },
                 '10': {
                     name: 'to be specified',
@@ -124,7 +143,7 @@ var canonnEd3d_tslinks = {
         ]
 	},
 	startcoords: [],
-	formatSites: async function (data) {
+	formatSites: (data) => {
         /*
         //not using the extensive json data as it seems to be older than the csv export from the gooogle sheet
         //also if link target is not thargoid site, it will be "null" while the csv mentions the system name
@@ -139,13 +158,12 @@ var canonnEd3d_tslinks = {
                 canonnEd3d_tslinks.sitesByIDs[data[i].siteID] = data[i];
             }
         }
-        
         //run through list and add sites and links at once, requires associative array with siteID as keys
-        for (var i = 0; i < canonnEd3d_tslinks.sitesByIDs.length; i++) {
-            let siteData = canonnEd3d_tslinks.sitesByIDs[i];
+        Object.keys(canonnEd3d_tslinks.sitesByIDs).forEach(async function(key, index) {
+            let siteData = this[key];
             //add the site
             let poiSite = {};
-            poiSite['name'] = siteData.system.systemName;
+            poiSite['name'] = siteData.system;
 
             //Check Site Type and match categories
             if (siteData.status == '✔') {
@@ -154,58 +172,73 @@ var canonnEd3d_tslinks = {
                 poiSite['cat'] = [202];
             }
             poiSite['coords'] = {
-                x: parseFloat(siteData.system.edsmCoordX),
-                y: parseFloat(siteData.system.edsmCoordY),
-                z: parseFloat(siteData.system.edsmCoordZ),
+                x: parseFloat(siteData.galacticX),
+                y: parseFloat(siteData.galacticY),
+                z: parseFloat(siteData.galacticZ),
             };
 
             // We can then push the site to the object that stores all systems
             canonnEd3d_tslinks.systemsData.systems.push(poiSite);
+            
+            canonnEd3d_tslinks.addRoute(siteData.system, siteData.msg1);
+            canonnEd3d_tslinks.addRoute(siteData.system, siteData.msg2);
+            canonnEd3d_tslinks.addRoute(siteData.system, siteData.msg3);
 
-            //add the route link
-            if (siteData['msg1'] && siteData['msg1'] != 'X' && siteData['msg1'].replace(' ', '').length > 1) {
-                //if its not a thargoid structure, we need to add the system, too
-                if ("TS" != siteData['msg1'].substr(0, 2)) {
-                    fetchAddSystem(siteData['msg1']);
-                }
-                addRoute(siteData, canonnEd3d_tslinks.sitesByIDs[siteData['msg1']]);
-            }
-            if (siteData['msg2'] && siteData['msg2'] != 'X' && siteData['msg2'].replace(' ', '').length > 1) {
-                //if its not a thargoid structure, we need to add the system, too
-                if ("TS" != siteData['msg2'].substr(0, 2)) {
-                    fetchAddSystem(siteData['msg2']);
-                }
-                addRoute(siteData, canonnEd3d_tslinks.sitesByIDs[siteData['msg2']]);
-            }
-            if (siteData['msg3'] && siteData['msg3'] != 'X' && siteData['msg3'].replace(' ', '').length > 1) {
-                //if its not a thargoid structure, we need to add the system, too
-                if ("TS" != siteData['msg3'].substr(0, 2)) {
-                    fetchAddSystem(siteData['msg3']);
-                }
-                addRoute(siteData, canonnEd3d_tslinks.sitesByIDs[siteData['msg3']]);
-            }
-        }
-
-		document.getElementById("loading").style.display = "none";
+        }, canonnEd3d_tslinks.sitesByIDs)
     },
     
-    fetchAddSystem: function(name) {
+    addedSystems: {},
+    fetchAddSystem: (name) => {
+        if (canonnEd3d_tslinks.addedSystems[name]) return;
+        getSystemEDSM(name).then(function(response) {
+            for (var i = 0; i < response.data.length; i++) {
+                
+                let system = response.data[i];
+                //add the site
+                let poiSite = {};
+                poiSite['name'] = system.name;
 
+                //Check Site Type and match categories
+                poiSite['cat'] = [10];
+                poiSite['coords'] = {
+                    x: parseFloat(system.coords.x),
+                    y: parseFloat(system.coords.y),
+                    z: parseFloat(system.coords.z),
+                };
+                canonnEd3d_tslinks.systemsData.systems.push(poiSite);
+                canonnEd3d_tslinks.addedSystems[system.name] = true;
+            }
+        });
+        
     },
+    
+    addRoute: (originSystem, msg) => {
 
-    //data is one line of canonn thargoid structure CSV data export (or tssites api call)
-    addRoute: function(originSite, targetSite) {
-        var route = {};
-        //todo 
-        route['cat'] = [10];
-        route['points'] = [
-            { 's': originSite.system, 'label': originSite.system },
-            { 's': targetSite.system, 'label': targetSite.system },
-        ];
-        route['circle'] = false;
+        //add the route link
+        if (msg && msg != 'X' && msg.replace(' ', '').length > 1) {
+            let cat = [10];
+            let tarname = msg;
+            //if its not a thargoid structure, we need to add the system, too
+            if ("TS" == msg.substr(0, 2)) {
+                tarname = canonnEd3d_tslinks.sitesByIDs[msg].system;
+                cat = [40];
+            } else {
+                canonnEd3d_tslinks.fetchAddSystem(msg);
+            }
 
-        // We can then push the site to the object that stores all systems
-        canonnEd3d_tslinks.systemsData.routes.push(route);
+            var route = {};
+            //todo 
+            route['cat'] = cat;
+            route['points'] = [
+                { 's': originSystem, 'label': originSystem },
+                { 's': tarname, 'label': tarname },
+            ];
+            route['circle'] = false;
+
+            // We can then push the site to the object that stores all systems
+            canonnEd3d_tslinks.systemsData.routes.push(route);
+        }
+
     },
 
 	parseCSVData: function (url, callBack, resolvePromise) {
@@ -232,7 +265,7 @@ var canonnEd3d_tslinks = {
 			canonnEd3d_tslinks.parseCSVData('data/csvCache/202011052200_Canonn Universal Science DB - TS Export - Export CSV Data.csv', canonnEd3d_tslinks.formatSites, resolve);
 		});
 
-		Promise.all([links]).then(function () {
+		Promise.resolve(links).then(function () {
 			Ed3d.init({
 				container: 'edmap',
 				json: canonnEd3d_tslinks.systemsData,
